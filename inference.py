4f2e67f002f1a35a150c8523ebe35f1e979a0ec4
Musab Gültekin <gultekinmusab@gmail.com>
2023-07-21 17:23:25 -0600

    Add Inference file that contains all inference logic

---
+++ inference.py
@@ -0,0 +1,93 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from schema import generate_schema_for_functions
+
+SYSTEM_MESSAGE = """A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. The assistant calls functions with appropriate input when necessary"""
+
+
+def prepare_message_for_inference(message):
+    """Prepares a given message for the model by tokenizing the content."""
+
+    if message["role"] == "system":
+        text = "system:\n{content}\n".format(content=message.get("content", ""))
+
+    elif message["role"] == "function":
+        text = "function name={name}:\n{content}\n".format(name=message.get("name", ""), content= message.get("content", ""))
+
+    elif message["role"] == "user" and message.get("content") is None:
+        text = "user:\n</s>"
+
+    elif message["role"] == "user":
+        text = "user:\n</s>{content}\n".format(content=message.get("content", ""))
+
+    elif message["role"] == "assistant" and message.get("to") is not None:
+        text = "assistant to={to}:\n{content}</s>".format(to=message.get("to", ""), content=message.get("content", ""))
+
+    elif message["role"] == "assistant" and message.get("content") is None:
+        text = "assistant"
+
+    elif message["role"] == "assistant":
+        text = "assistant:\n{content}\n".format(content=message.get("content", ""))
+
+    else:
+        raise ValueError(f'Unsupported role: {message["role"]}')
+
+    input_ids = tokenizer(text, add_special_tokens=False, return_tensors="pt").input_ids.to(device)
+    return input_ids
+
+
+def prepare_messages_for_inference(messages, functions=None):
+    all_messages = []
+    if functions is not None:
+        all_messages.append({"role": "system", "content": generate_schema_for_functions(functions)})
+    all_messages.append({"role": "system", "content": SYSTEM_MESSAGE})
+    all_messages.extend(messages)
+    all_messages.append({"role": "assistant", "content": None})
+    all_input_ids = [prepare_message_for_inference(msg) for msg in messages]
+    return torch.cat(all_input_ids, dim=-1)
+
+
+def generate(messages, functions=None, temperature=0.7, max_new_tokens=256):
+    inputs = prepare_messages_for_inference(messages, functions)
+    generate_ids = model.generate(inputs, max_new_tokens=max_new_tokens, temperature=temperature)
+    return tokenizer.batch_decode(generate_ids[:, inputs.shape[1]:], skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+
+
+if __name__ == "__main__":
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    model = AutoModelForCausalLM.from_pretrained("musabgultekin/functionary-7b-v0.2", low_cpu_mem_usage=True, torch_dtype=torch.float16).to(device)
+    tokenizer = AutoTokenizer.from_pretrained("musabgultekin/functionary-7b-v0.2", use_fast=False)
+
+    generate(
+        messages=[
+            {"role": "user", "content": "what is the weather for istanbul?"},
+            {"role": "assistant", "to": "functions.get_current_weather", "content": '{\n  "location": "Istanbul",\n  "format": "celsius"\n}'},
+            {"role": "function", "name": "functions.get_current_weather", "content": '{"value": 32}'},
+            {"role": "assistant", "content": "The current weather in Istanbul is 32 degrees Celsius."},
+            {"role": "user", "content": "what is the weather for san francisco?"},
+        ], 
+        functions=[
+                {
+                    "name": "get_current_weather",
+                    "description": "Get the current weather",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",
+                            },
+                            "format": {
+                                "type": "string",
+                                "enum": ["celsius", "fahrenheit"],
+                                "description": "The temperature unit to use. Infer this from the users location.",
+                            },
+                        },
+                        "required": ["location", "format"],
+                    },
+                },
+        ]
+    )
+    
+    
