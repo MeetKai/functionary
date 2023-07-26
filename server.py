5f15e050364064d1901085cf18e7a8a38388d54e
Kyle Kelley <rgbkrk@gmail.com>
2023-07-26 14:35:59 -0700

    support Apple Metal Performance Shaders (MPS)
    I let black and isort do their thing so I'll have to squash this down to
    simple commits shortly.

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
-from typing import List, Dict, Any, Optional
-from pydantic import BaseModel
-import uuid
-import time
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-from inference import generate
-import uvicorn
 import argparse
+import time
+import uuid
+from typing import Any, Dict, List, Optional
+
+import torch
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from inference import generate
 
 app = FastAPI()
 
@@ -20,29 +22,39 @@ class ChatInput(BaseModel):
 
 @app.post("/v1/chat/completions")
 async def chat_endpoint(chat_input: ChatInput):
-    generated_message = generate(model, tokenizer, chat_input.messages, chat_input.functions, chat_input.temperature)
+    generated_message = generate(
+        model,
+        tokenizer,
+        chat_input.messages,
+        chat_input.functions,
+        chat_input.temperature,
+    )
 
     return {
-        'id': str(uuid.uuid4()),
-        'object': 'chat.completion',
-        'created': int(time.time()),
-        'model': model_name,
-        'choices': [
-            {
-                'message': generated_message,
-                'finish_reason': 'stop',
-                'index': 0
-            }
-        ]
+        "id": str(uuid.uuid4()),
+        "object": "chat.completion",
+        "created": int(time.time()),
+        "model": model_name,
+        "choices": [
+            {"message": generated_message, "finish_reason": "stop", "index": 0}
+        ],
     }
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Functionary API Server")
-    parser.add_argument('--model', type=str, default='musabgultekin/functionary-7b-v1', help='The model name to be used.')
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="musabgultekin/functionary-7b-v1",
+        help="The model name to be used.",
+    )
     args = parser.parse_args()
 
     model_name = args.model
-    model = AutoModelForCausalLM.from_pretrained(model_name, low_cpu_mem_usage=True, torch_dtype=torch.float16).to("cuda:0")
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, low_cpu_mem_usage=True, torch_dtype=torch.float16
+    ).to("mps:0")
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
