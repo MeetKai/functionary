ad37191a1527f3a0ce80fe8d60b4a6d481e38ec5
Jeffrey Fong <jeffrey.fong@meetkai.com>
2023-09-20 12:11:42 +0000

    set up rope scaling

---
+++ train/train.sh
@@ -0,0 +1,22 @@
+torchrun --nproc_per_node=2 --master_port=20001 train.py \
+    --model_name_or_path /workspace/Llama-2-7B-fp16  \
+    --data_path llama_training_dataset.jsonl \
+    --bf16 True \
+    --num_train_epochs 2 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 200 \
+    --save_total_limit 5 \
+    --learning_rate 2e-5 \
+    --weight_decay 0.3 \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True \
+    --model_max_length 4096 \
+    --gradient_checkpointing True \
+    --output_dir functionary-v1
