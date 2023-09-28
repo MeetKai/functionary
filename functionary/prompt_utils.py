44e637b17a20c0225dc0f5e24a3a8171883b4891
Khai Mai <khai.mai@meetkai.com>
2023-09-28 11:18:38 +0700

    refactor and add more description, comments to prompt_utils.py

--- functionary/prompt_utils.py
+++ functionary/prompt_utils.py
@@ -1,20 +1,53 @@
-from transformers import LlamaTokenizer
 from typing import List, Dict, Any, Optional, Tuple
 import torch
+from enum import Enum
 
 
-ROLE_MAPPING = {
-    "user": "<|END_OF_USER|>",
-    "system": "<|END_OF_SYSTEM|>",
-    "assistant": "<|END_OF_ASSISTANT|>",
-    "function": "<|END_OF_FUNCTION|>",
-}
+class EndToken(str, Enum):
+    system = "<|END_OF_SYSTEM|>"
+    user = "<|END_OF_USER|>"
+    assistant = "<|END_OF_ASSISTANT|>"
+    function = "<|END_OF_FUNCTION_RESULT|>"
+    function_call = "<|END_OF_FUNCTION_CALL|>"
+
+
+def get_end_token_for_message(message: Dict) -> EndToken:
+    """this function is used for getting the end token for each message.
+    For example, if message["role"] == "user" --> return EndToken.user
+    if message["role"] == "assistant" and "function_call" in message --> EndTOken.function_call
+
+    Args:
+        message (Dict): A dictionary containing: role, content, function_call(optional)
+
+    Returns:
+        EndToken: End Token for this message, this will be appended to the end of the prompt for this message
+    """
+    role = message["role"]
+    if role == "user":
+        return EndToken.user
+    elif role == "system":
+        return EndToken.system
+    elif role == "function":
+        return EndToken.function
+    else:  # role = assistant
+        if "function_call" in message:
+            return EndToken.function_call
+        else:
+            return EndToken.assistant
 
 
 def get_text_from_message(message: Dict) -> str:
-    """Prepares a given message for the model by tokenizing the content and determining target tokens."""
-    stop_token = ROLE_MAPPING[message["role"]]
+    """convert a message to a string to be included in the prompt
+
+    Args:
+        message (Dict): A dictionary in OpenAI format (containing: role, content, function_call (optional))
+
+    Returns:
+        str: the string used in the final prompt of this message
+    """
+    stop_token = get_end_token_for_message(message).value
     content = message.get("content", "")
+
     if content is not None:
         content = f"{content}{stop_token}"
 
@@ -29,22 +62,16 @@ def get_text_from_message(message: Dict) -> str:
 
     elif message["role"] == "user":
         text = "user:\n{content}\n".format(content=content)
+
     elif message["role"] == "assistant":
         function = None
         arguments = None
         if (
-            "to" in message
-        ):  # if "to" in message --> format of Musab: {"role": "assistant", "to": func_name, "content": arguments}
-            function = message["to"]
-            arguments = content
-        elif (
             "function_call" in message
         ):  # format of openai: {"role": assistant, "function_call": {"name": xxx, "arguments": xxx}}
             assert content is None
             function = message["function_call"]["name"]
             arguments = message["function_call"]["arguments"] + stop_token
-
-        if function is not None:  # function call
             text = f"assistant to={function}:\n{arguments}\n"
         elif content is not None:  # this is text content
             text = f"assistant:\n{content}\n"
@@ -54,10 +81,12 @@ def get_text_from_message(message: Dict) -> str:
     # This chunk of code below is to assert that stop_token is added correctly
     no_stop_token = False
     if content is None:
-        if message["role"] == "user":
+        if message["role"] == "user":  # if content is None and role=user --> no need stop-token
             no_stop_token = True
             assert not text.endswith(stop_token)
-        elif message["role"] == "assistant":  # used at inference
+        elif (
+            message["role"] == "assistant"
+        ):  # used at inference when message = {"role": "assistant", "content": None} --> no need to add stop-token
             if "function_call" not in message and "to" not in message:
                 no_stop_token = True
                 assert not text.endswith(stop_token)
@@ -67,24 +96,54 @@ def get_text_from_message(message: Dict) -> str:
     return text
 
 
+def convert_old_format_to_openai_format(message: Dict) -> Dict:
+    """convert {"to": xxx, "content": xxx} --> {"function_call": {"name": xxx, "arguments": xxx}}
+
+    Args:
+        message (Dict): dictionary in old format ({"to": xxx, "content": xxx})
+
+    Returns:
+        Dict: dictionary in openai format
+    """
+    if "to" not in message:
+        return message
+    return {
+        "role": message["role"],
+        "content": None,
+        "function_call": {"name": message["to"], "arguments": message["content"]},
+    }
+
+
 def get_prompt_from_messages(messages: List[Dict]) -> str:
+    """return the final prompt that will be used.
+    Args:
+        messages (List[Dict]): list of messages where each message is in the format of OpenAI
+
+    Returns:
+        str: the final prompt that will be used.
+    """
     result = ""
     for mess in messages:
         result += get_text_from_message(mess)
     return result
 
 
-def get_role_token_id_map(tokenizer: Any) -> Dict:
+def get_token_id_to_end_token(tokenizer: Any) -> Dict[int, EndToken]:
+    """return a dictionary mapping from token_id --> end_token
+
+    Args:
+        tokenizer (Any): tokenizer in transformers
+
+    Returns:
+        Dict[int, EndToken]: the mapping from token_id --> end_token
+    """
     result = {}
-    for role, role_stop_token in ROLE_MAPPING.items():
-        tok_ids = tokenizer.encode(role_stop_token, add_special_tokens=False)
+    for item in EndToken:
+        tok_ids = tokenizer.encode(item.value, add_special_tokens=False)
         assert len(tok_ids) <= 2
         if len(tok_ids) == 2:
-            assert tok_ids[0] == 29871  # tokenizer add this token intentionally
-        tok_id = tok_ids[0]
-        if len(tok_ids) == 2:
-            tok_id = tok_ids[1]
-        result[tok_id] = role
+            assert tok_ids[0] == 29871  # Llama tokenizer adds this token intentionally
+        result[tok_ids[-1]] = item
     return result
 
 
@@ -96,89 +155,56 @@ def prepare_training_inputs(
     return_tensor: bool = True,
     verbose=False,
 ) -> Tuple[str, Dict]:
-    stop_id_to_role = get_role_token_id_map(tokenizer)
-    prompt_str = get_prompt_from_messages(messages)
+    """This function is used for when you want to get a dictionary input for the model.forward.
+    The dictionary will contain: input_ids, attention_maks, labels.
+    labels is like input_ids except that content from user, system, function will be set as -100, only content from assistant remains
+
+    Args:
+        messages (List[Dict]): List of messages in openAI format (containing: role, content and function_call (optional))
+        tokenizer (Any): tokenizer from transformers
+        padding (str, optional): type of padding (longest, max_length), this is passed to tokenizer(). Defaults to "max_length".
+        max_length (Optional[int], optional): maximum number of tokens allowed in prompt. Defaults to None.
+        return_tensor (bool, optional): if true, the input_dic will be dictionary[str, Tensor] else dictionary[str, List[int]]. Defaults to True.
+        verbose (bool, optional): to print some useful information or not. Defaults to False.
+
+    Returns:
+        Tuple[str, Dict]: (final_prompt_str, inputs)
+            final_prompt_str: the final prompt to be used,
+            inputs: a dictionary containing: input_ids, attention_mask, labels. This will be used in model.forward(**inputs)
+    """
+    # a dictionary mapping from token_id --> end_token
+    id_to_endtoken = get_token_id_to_end_token(tokenizer)
+    prompt_str = get_prompt_from_messages(messages)  # prompt_str is the concatenation of all prompts from messages
     max_length = max_length if max_length is not None else tokenizer.model_max_length
+
     input_dic = tokenizer(prompt_str, padding=padding, max_length=max_length, truncation=True)
     input_token_ids = input_dic["input_ids"]
-    start = 0
+    # first we initialize labels with all positions as -100,
+    # then we will fill in positions where role=assistant as we only include these in computing the loss
     labels = [-100 for _ in range(len(input_token_ids))]
+    start = 0
+    # Now we find the positions where role=assistant and copy the value from input_token_ids
+    # this is done by finding the chunk (previous_stop_token_index + 1, current_stop_token_index + 1)
+    # where current_stop_token is EndToken.assistant or EndToken.function_call
     for index, tok_id in enumerate(input_token_ids):
-        if tok_id in stop_id_to_role:
-            role = stop_id_to_role[tok_id]
-            if role == "assistant":  # only compute loss from tokens of assistant
+        if tok_id in id_to_endtoken:  # Find position of end_token in final_prompt
+            end_token = id_to_endtoken[tok_id]
+            if (
+                end_token == EndToken.assistant or end_token == EndToken.function_call
+            ):  # only compute loss from tokens of assistant
                 for i in range(start + 2, index + 1):  # The reason for start + 2 is to ignore: "\nassistant" (2 tokens)
-                    labels[i] = input_token_ids[i]
+                    labels[i] = input_token_ids[i]  # overwrite -100 to compute the loss
                 if verbose:
                     chunk = input_token_ids[start + 2 : index + 1]
+                    print("----------------------------")
                     print("+++ chunk assistant to compute loss: ", tokenizer.decode(chunk))
                     print("chunk tokens: ", chunk)
             start = index + 1
+
     input_dic["labels"] = labels
     assert len(labels) == len(input_dic["input_ids"]) == len(input_dic["attention_mask"])
+
     if return_tensor:
         for key in input_dic:
             input_dic[key] = torch.tensor(input_dic[key])
     return prompt_str, input_dic
-
-
-def get_test_messages_musab_format():
-    return [
-        {"role": "system", "content": "This is the conversation between AI and HUman"},
-        {"role": "user", "content": "what is the definition of mammal?"},
-        {"role": "assistant", "content": "mammal is a vertebrate animal of the class Mammalia"},
-        {"role": "user", "content": "what is the weather in Hanoi now"},
-        {"role": "assistant", "to": "call_weather_check", "content": '{"city": "Hanoi"}'},
-        {
-            "role": "function",
-            "name": "call_weather_check",
-            "content": '{"weather_info": "The weather is cool, temperature is about 20 Degree Celcius"}',
-        },
-        {"role": "assistant", "content": "The weather in Hanoi is cool, about 20 Degree Celcius"},
-        {"role": "user", "content": "Thank you"},
-        {"role": "assistant", "content": None},
-    ]
-
-
-def get_test_messages_openai_format():
-    return [
-        {"role": "user", "content": "is the car Song more expensive than car Tang?"},
-        {
-            "role": "assistant",
-            "content": None,
-            "function_call": {"name": "get_car_price", "arguments": '{\n  "car_name": "Song"\n}'},
-        },
-        {"role": "function", "content": "{'price': {'price': '$25000'}}", "name": "get_car_price"},
-        {
-            "role": "assistant",
-            "content": None,
-            "function_call": {"name": "get_car_price", "arguments": '{\n  "car_name": "Tang"\n}'},
-        },
-        {"role": "function", "content": "{'price': {'price': '$20000'}}", "name": "get_car_price"},
-        {
-            "role": "assistant",
-            "content": "No, the car Tang is less expensive than the car Song. The car Song is priced at $25,000, while the car Tang is priced at $20,000.",
-        },
-    ]
-
-
-def test_prompt():
-    tokenizer = LlamaTokenizer.from_pretrained("musabgultekin/functionary-7b-v1")
-    added_tokens = list(ROLE_MAPPING.values())
-    print("token_size: ", len(tokenizer))
-    print("added_tokens: ", added_tokens)
-    tokenizer.add_tokens(added_tokens)
-    print("token_size after: ", len(tokenizer))
-    messages1 = get_test_messages_musab_format()
-    messages2 = get_test_messages_openai_format()
-    for messages in [messages1, messages2]:
-        print("--------------------------------------")
-        prompt_str, input_dic = prepare_training_inputs(
-            messages, tokenizer, padding="longest", max_length=200, verbose=True
-        )
-        print(f"prompt_str\n{prompt_str}")
-        print("input_dic: ", input_dic)
-
-
-if __name__ == "__main__":
-    test_prompt()
