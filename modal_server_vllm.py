4bbd60b2bc53c211d0efd783308793f4c7250841
Jeffrey Fong <jeffreyfong94@gmail.com>
2024-11-15 15:59:23 +0800

    update modal

--- modal_server_vllm.py
+++ modal_server_vllm.py
@@ -7,27 +7,30 @@ from typing import Annotated, Dict, List, Literal, Optional, Union
 import modal
 from fastapi import FastAPI, Header, Request
 from fastapi.responses import JSONResponse
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from functionary.openai_types import ChatCompletionRequest
-from modal_server_config import Settings
 
 app = modal.App("functionary_vllm")
 fast_api_app = FastAPI(title="Functionary API")
+
+
+class Settings(BaseSettings):
+    model: str = "meetkai/functionary-small-v2.5"
+    max_model_length: int = 8192
+    gpu_type: Literal["l4", "t4", "a10g", "a100-40gb", "a100-80gb", "h100"] = "a10g"
+    gpu_count: int = 1
+    gpu_memory_utilization: float = 0.9
+    storage_model_dir: str = "/model"
+    execution_timeout: int = 1200  # Generation timeout in seconds
+    container_idle_timeout: int = 1200  # In seconds
+    batch_size_per_container: int = 10
+    enable_grammar_sampling: bool = False
+
+    model_config = SettingsConfigDict(env_prefix="modal_")
+
+
 settings = Settings()
-
-
-def download_model_to_image(model_dir, model_name):
-    from huggingface_hub import snapshot_download
-    from transformers.utils import move_cache
-
-    os.makedirs(model_dir, exist_ok=True)
-
-    snapshot_download(
-        model_name,
-        local_dir=model_dir,
-        ignore_patterns=["*.pt", "*.bin"],  # Using safetensors
-    )
-    move_cache()
 
 
 def get_model():
@@ -43,8 +46,8 @@ def get_model():
         from vllm.engine.async_llm_engine import AsyncLLMEngine
 
     engine_args = AsyncEngineArgs(
-        model=settings.storage_model_dir,
-        tensor_parallel_size=settings.gpu_config.count,
+        model=settings.model,
+        tensor_parallel_size=settings.gpu_count,
         gpu_memory_utilization=settings.gpu_memory_utilization,
         enforce_eager=False,  # capture the graph for faster inference, but slower cold starts
         disable_log_stats=True,  # disable logging so we can stream tokens
@@ -69,25 +72,49 @@ def get_model():
 image = (
     modal.Image.debian_slim()
     .pip_install(
-        "hf-transfer==0.1.6", "huggingface_hub==0.22.2", "pydantic-settings==2.2.1"
+        "hf-transfer==0.1.8", "huggingface_hub==0.26.2", "pydantic-settings==2.6.1"
     )
-    .pip_install_from_requirements("requirements.txt")
-    .run_function(
-        download_model_to_image,
-        timeout=60 * 20,
-        kwargs={"model_dir": settings.storage_model_dir, "model_name": settings.model},
+    .pip_install_from_pyproject("pyproject.toml", optional_dependencies=["vllm"])
+    .env(
+        {
+            "HF_HUB_ENABLE_HF_TRANSFER": "1",
+            "HF_TOKEN": os.environ["HF_TOKEN"],
+            "MODAL_MODEL": settings.model,
+            "MODAL_MAX_MODEL_LENGTH": str(settings.max_model_length),
+            "MODAL_GPU_TYPE": settings.gpu_type,
+            "MODAL_GPU_COUNT": str(settings.gpu_count),
+            "MODAL_GPU_MEMORY_UTILIZATION": str(settings.gpu_memory_utilization),
+            "MODAL_STORAGE_MODEL_DIR": settings.storage_model_dir,
+            "MODAL_EXECUTION_TIMEOUT": str(settings.execution_timeout),
+            "MODAL_CONTAINER_IDLE_TIMEOUT": str(settings.container_idle_timeout),
+            "MODAL_BATCH_SIZE_PER_CONTAINER": str(settings.batch_size_per_container),
+            "MODAL_ENABLE_GRAMMAR_SAMPLING": str(
+                settings.enable_grammar_sampling
+            ).lower(),
+        }
     )
 )
 
 
 @app.cls(
-    gpu=settings.gpu_config,
+    image=image,
+    gpu=f"{settings.gpu_type}:{settings.gpu_count}",
     timeout=settings.execution_timeout,
     container_idle_timeout=settings.container_idle_timeout,
     allow_concurrent_inputs=settings.batch_size_per_container,
-    image=image,
 )
 class Model:
+    @modal.build()
+    def download_model(self):
+        from huggingface_hub import snapshot_download
+        from transformers.utils import move_cache
+
+        snapshot_download(
+            settings.model,
+            ignore_patterns=["*.pt", "*.bin"],  # Using safetensors
+        )
+        move_cache()
+
     @modal.enter()
     def start_engine(self):
         model, tokenizer, engine_model_config = get_model()
@@ -104,6 +131,7 @@ class Model:
             raw_request=None,
             tokenizer=self.tokenizer,
             served_model=settings.model,
+            served_loras=[],
             engine_model_config=self.engine_model_config,
             enable_grammar_sampling=settings.enable_grammar_sampling,
             engine=self.model,
@@ -111,7 +139,7 @@ class Model:
 
     @modal.exit()
     def stop_engine(self):
-        if settings.gpu_config.count > 1:
+        if settings.gpu_count > 1:
             import ray
 
             ray.shutdown()
