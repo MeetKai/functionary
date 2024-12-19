6876d75f3a81cbb528da59b124209e3f6848c219
Khai Mai <khai.mai@meetkai.com>
2024-12-19 11:21:24 +0700

    add a template for activating reasoning

---
+++ functionary/prompt_template/llama31_reasoning_promp_template.py
@@ -0,0 +1,7 @@
+import datetime
+
+from functionary.prompt_template.llama31_prompt_template import Llama31Template
+
+
+class Llama31ReasoningTemplate(Llama31Template):
+    version = "v3-llama3.1-reasoning"
