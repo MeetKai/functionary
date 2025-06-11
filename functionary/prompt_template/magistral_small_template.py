ee7abaed899f137fb4bc58037468b5da3dd12e83
Khai Mai <khai.mai@meetkai.com>
2025-06-11 10:24:20 +0700

    add Magistral_small

---
+++ functionary/prompt_template/magistral_small_template.py
@@ -0,0 +1,28 @@
+from functionary.prompt_template.base_template import PromptTemplate
+from typing import Any, Dict, List, Literal, Optional, Tuple, Union
+from functionary.prompt_template import prompt_utils
+from functionary.openai_types import Function, Tool
+import json
+import copy
+import math
+import re
+
+
+class MagistralSmallPromptTemplate(PromptTemplate):
+    version = "magistral_small"
+    
+    def get_chat_template_jinja(self) -> str:
+        path_prefix = "./functionary/prompt_template/jinja_templates/"
+        with open(f"{path_prefix}{self.version}.txt", "r") as f:
+            template = f.read()
+
+        return template
+    
+    def get_additional_tokens(self) -> List[str]:
+        return []
+
+    def get_assistant_prefixes(self) -> List[str]:
+        return [f"[/INST]"]
+
+    def get_stop_tokens_for_generation(self) -> List[str]:
+        return ["</s>"]
